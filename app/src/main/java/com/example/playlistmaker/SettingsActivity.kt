7f54a11fc8b58ad321bac8ca05a5fb6ded673074
i.letavin <i.letavin@vk.team>
2026-08-09 19:03:19 +0300

    Верстка экранов: - Главный экран - Экран Настроек

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
@@ -21,8 +20,7 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun openMainScreen() {
-        val mainIntent = Intent(this, MainActivity::class.java)
-        startActivity(mainIntent)
+        finish()
     }
 
     fun initViews() {
