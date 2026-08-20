4752a71c617c6c18d28848defc325128e929ff25
i.letavin <i.letavin@vk.team>
2026-08-21 00:00:31 +0300

    Логика экрана Настроек и Поиска. Правки верстки

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,18 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 
 class SettingsActivity : AppCompatActivity() {
 
     lateinit var backBtnToolbar: ImageButton
+    lateinit var shareAppBtn: TextView
+    lateinit var supportBtn: TextView
+    lateinit var userAgreementBtn: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +23,71 @@ class SettingsActivity : AppCompatActivity() {
         backBtnToolbar.setOnClickListener {
             openMainScreen()
         }
+
+        shareAppBtn.setOnClickListener {
+            shareApp()
+        }
+
+        supportBtn.setOnClickListener {
+            writeSupport()
+        }
+
+        userAgreementBtn.setOnClickListener {
+            openUserAgreement()
+        }
+
     }
 
-    fun openMainScreen() {
+    private fun shareApp() {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+        shareIntent.putExtra(
+            Intent.EXTRA_TEXT,
+            getString(R.string.course_url
+            )
+        )
+
+        startActivity(
+            Intent.createChooser(
+                shareIntent,
+                getString(R.string.share)
+            )
+        )
+    }
+
+    private fun writeSupport() {
+        val writeIntent = Intent(Intent.ACTION_SENDTO)
+        val topic = getString(R.string.topic_support)
+        val message = getString(R.string.support_message)
+
+        writeIntent.data = "mailto:".toUri()
+        writeIntent.putExtra(
+            Intent.EXTRA_EMAIL,
+            arrayOf(getString(R.string.student_url)
+            )
+        )
+        writeIntent.putExtra(Intent.EXTRA_SUBJECT, topic)
+        writeIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+        startActivity(writeIntent)
+    }
+
+    private fun openUserAgreement() {
+        val userAgreementIntent = Intent(
+            Intent.ACTION_VIEW,
+            getString(R.string.user_agreement_url).toUri()
+        )
+        startActivity(userAgreementIntent)
+    }
+
+    private fun openMainScreen() {
         finish()
     }
 
-    fun initViews() {
+    private fun initViews() {
         backBtnToolbar = findViewById(R.id.back_btn_toolbar)
+        shareAppBtn = findViewById<TextView>(R.id.shareAppTextView)
+        supportBtn = findViewById<TextView>(R.id.writeSupportTextView)
+        userAgreementBtn = findViewById<TextView>(R.id.userAgreementTextView)
     }
 }
