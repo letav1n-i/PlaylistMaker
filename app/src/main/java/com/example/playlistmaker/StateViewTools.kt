6968ba45ba136e1dec0d8157a179f3aa7635acb9
i.letavin <i.letavin@vk.team>
2026-09-07 22:46:49 +0300

    Фикс мерджа

--- app/src/main/java/com/example/playlistmaker/StateViewTools.kt
+++ app/src/main/java/com/example/playlistmaker/StateViewTools.kt
@@ -1,7 +1,9 @@
 package com.example.playlistmaker
 
 import android.app.Activity
+import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
+import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -20,6 +22,14 @@ class StateViewTools {
             val imm = activity
                 .getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        fun dpToPx(dp: Float, context: Context): Int {
+            return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dp,
+                context.resources.displayMetrics
+            ).toInt()
         }
     }
 }
